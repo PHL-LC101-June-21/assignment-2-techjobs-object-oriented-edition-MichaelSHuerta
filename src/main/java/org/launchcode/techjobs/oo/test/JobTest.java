b686f9b54c3f4094836cf2360b67eb566f94af87
MichaelSHuerta <michaelhuerta1997@gmail.com>
2021-10-24 21:53:33 -0400

    update part 4

--- src/main/java/org/launchcode/techjobs/oo/test/JobTest.java
+++ src/main/java/org/launchcode/techjobs/oo/test/JobTest.java
@@ -1,7 +1,11 @@
 package org.launchcode.techjobs.oo.test;
 
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
+import org.launchcode.techjobs.oo.*;
+
 
 /**
  * Created by LaunchCode
@@ -9,4 +13,21 @@ import org.junit.runners.JUnit4;
 @RunWith(JUnit4.class)
 public class JobTest {
 
+     Job testJob;
+     Job testJobTwo;
+     Job test_job3;
+
+@Test
+public void testSettingJobId(){
+    assertEquals(testJob, testJobTwo);
+    }
+@Test
+    public void testJobConstructorSetsAllFields(){
+    test_job3 =  new Job("Product tester",
+            new Employer("ACME"),
+            new Location("Desert"),
+            new PositionType("Quality control"),
+            new CoreCompetency("Persistence"));
+
+}
 }
